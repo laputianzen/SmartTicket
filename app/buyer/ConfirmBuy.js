f481bbc3bebd101af4e161bfbef135f2728ee69c
Peter Tsai <laputianzen@gmail.com>
2016-08-19 21:32:21 +0800

    update confirmBuy template helpers

--- app/buyer/ConfirmBuy.js
+++ app/buyer/ConfirmBuy.js
@@ -1,12 +1,19 @@
 if (Meteor.isClient) {
 Template.ConfirmBuy.helpers({
+	ConcertName: function () {return buyContractInstance.name();},
 	AccountBalance: function(){
 		var user = web3.eth.accounts[0];
 		return web3.fromWei(web3.eth.getBalance(user),"ether").round("2");
 
 	},
-	TicketCost: function(){
-
+	TicketPrice: function(){ return Session.get('TicketPrice');},
+	TicketAmount: function(){ return Session.get('TicketNumber');},
+	TicketCost: function(){  return Math.floor(Session.get('TicketPrice')) * Session.get('TicketNumber');},
+	RemainingBalance: function() { 
+		var user = web3.eth.accounts[0];
+		var originalBalance = web3.fromWei(web3.eth.getBalance(user),"ether")
+		var ticketCost = Math.floor(Session.get('TicketPrice')) * Session.get('TicketNumber');
+		return originalBalance - ticketCost;
 	},
 });
 
@@ -19,8 +26,8 @@ Template.ConfirmBuy.events({
 	'submit form': function(event){
 	    event.preventDefault();
 	    /////its two in this case 
-	    var price = event.target.yes.value*2;
-	    console.log(price);
+	    var price = event.target.ticketNumber.value * Session.get('TicketPrice');
+	    //console.log(price);
 	    //starting transcation, alert when transaction finished
 	    var Cost = web3.toWei(price, "ether");
 	    web3.eth.sendTransaction({from: web3.eth.accounts[0], to: Session.get('ConcertInstance'), value: Cost});
