7ec525ace0079b641b79bb30be5d7f6844b02261
Peter Tsai <laputianzen@gmail.com>
2016-08-19 21:28:06 +0800

    add template Buy helpers function

--- app/buyer/Buy.js
+++ app/buyer/Buy.js
@@ -1,12 +1,14 @@
 if (Meteor.isClient) {
 // Mumin hack
 Template.Buy.helpers({
-	Host: function(){},
-	Place: function(){},
-	StartTime: function(){},
-	EndTime: function(){},
-	TicketPrice: function(){},
-	TicketAvailable: function(){},
+	ConcertName: function() {return buyContractInstance.name()},
+	Host: function(){return 'No Stored in Smart Contract';},
+	Place: function(){return buyContractInstance.place()},
+	WithDrawalDeadline: function(){return buyContractInstance.withDrawalDeadline()},
+	StartTime: function(){return buyContractInstance.startTime()},
+	EndTime: function(){return buyContractInstance.endTime()},
+	TicketPrice: function(){return buyContractInstance.ticketPrice()},
+	TicketAvailable: function(){return buyContractInstance.ticketSupply()},
 
 })
 
@@ -16,15 +18,17 @@ Template.Buy.events({
 	        BlazeLayout.render('mainLayout', {main: "SelectConcert"});
 
 	    },   
-	'click .buy':function(event){
+	'submit form':function(event){
 		event.preventDefault();
 		//session set a lot of things, not done
-		Session.set('Host',SmartContractIntance.name());
-		Session.set('Place',SmartContractInstance.place());
-		Session.set('StartTime',SmartContractIntance.startTime());
-		Session.set('EndTime',SmartContractInstance.endTime());
-		Session.set('TicketPrice',SmartContractIntance.endTime());
-		Session.set('TicketAvailable', SmartContractInstance.ticketPrice());
+		Session.set('TicketNumber', event.target.ticketNumber.value);
+
+		Session.set('Host',buyContractInstance.name());
+		Session.set('Place',buyContractInstance.place());
+		Session.set('StartTime',buyContractInstance.startTime().toString());
+		Session.set('EndTime',buyContractInstance.endTime().toString());
+		Session.set('TicketPrice',buyContractInstance.ticketPrice().toString());
+		Session.set('TicketAvailable', buyContractInstance.ticketSupply().toString());
 		BlazeLayout.render('mainLayout', {main: "ConfirmBuy"});
 	}
 });
