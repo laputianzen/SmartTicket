f88ab2176600032e13b65e4ca3fab5e15dee592f
Peter Tsai <laputianzen@gmail.com>
2016-08-20 18:48:21 +0800

    Create ConfirmEvent Template

---
+++ app/supervisor/ConfirmEvent.js
@@ -0,0 +1,44 @@
+if (Meteor.isClient) {
+
+Template.ConfirmEvent.helpers({
+	Confirmation: function () {
+		var confirmSignal = Session.get('confirmSignal');
+		console.log(typeof confirmSignal);
+		console.log(confirmSignal);
+		if (confirmSignal){
+			return 'Confrimation';
+		} else {
+			return 'Denial';
+		}
+	},
+});
+
+Template.ConfirmEvent.events({
+	'click .back': function(event){
+		event.preventDefault();
+	        BlazeLayout.render('mainLayout', {main: "Supervise"});
+	},
+	'submit form': function(event){
+	    event.preventDefault();	 
+	    var account = web3.eth.accounts[0];
+	    var password = event.target.password.value;  
+
+	   	if(! web3.personal.unlockAccount(account,password)){
+			alert('Account ' + account + 'cannot unlock');
+		} else {
+			alert('Account unlock!!!');
+		}
+
+		superviseContractInstance.confirmEvent({},{from: account, gas: 100000});
+/*		superviseContractInstance.ConfirmEvent({},{address:account}).watch(function(error, log){
+	    	if (!error) {
+	    		var superviseContractAddress = superviseContractInstnace.address
+	    		alert("Event " + superviseContractAddress + " accepts your " + logs.arg.stateName
+	    		);
+	    	}
+	    });
+*/
+	},
+});
+
+}
