f3f986b03786b54527ae327e2f9db8eeebcdf4ad
Peter Tsai <laputianzen@gmail.com>
2016-08-20 13:45:40 +0800

    update confirmRefund Functionality

--- app/buyer/ConfirmRefund.js
+++ app/buyer/ConfirmRefund.js
@@ -7,7 +7,25 @@ Template.ConfirmRefund.events({
 	'click .no': function(event){
 		event.preventDefault();
 	        BlazeLayout.render('mainLayout', {main: "Buyer"});
+	},
+	'submit form': function(event){
+	    event.preventDefault();	 
+	    var account = web3.eth.accounts[2];
+	    var password = event.target.password.value;  
 
+	   	if(! web3.personal.unlockAccount(account,password)){
+			alert('Account ' + account + 'cannot unlock');
+		} else {
+			alert('Account unlock!!!');
+		}
+
+		buyContractInstance.refund({},{from: account, gas: 100000});
+/*		buyContractInstance.Refund({},{address:account}).watch(function(error, log){
+	    	if (!error) {
+	    		alert("You got " + log.args.refundAmount + "ether refund! \n");
+	    	}
+	    });
+*/
 	},
 });
 
