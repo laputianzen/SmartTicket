11aeaba01c3012e215550e510bd86c6e2fde13eb
Peter Tsai <laputianzen@gmail.com>
2016-08-20 18:48:43 +0800

    Create GetIncome Template

---
+++ app/seller/GetIncome.js
@@ -0,0 +1,38 @@
+if (Meteor.isClient) {
+
+Template.GetIncome.helpers({
+	ConcertName: function () {return sellContractInstance.name();},
+});
+
+Template.GetIncome.events({
+	'click .no': function(event){
+		event.preventDefault();
+	        BlazeLayout.render('mainLayout', {main: "YourExistingConcert"});
+	},
+	'submit form': function(event){
+	    event.preventDefault();	 
+	    var account = web3.eth.accounts[0];
+	    var password = event.target.password.value;  
+
+	   	if(! web3.personal.unlockAccount(account,password)){
+			alert('Account ' + account + 'cannot unlock');
+		} else {
+			alert('Account unlock!!!');
+		}
+
+		sellContractInstance.getIncome({},{from: account, gas: 100000});
+/*		sellContractInstance.GetIncome({},{address:account}).watch(function(error, log){
+	    	if (!error) {
+	    		var sellContractAddress = sellContractInstnace.address
+	    		alert("You sold " + log.args.soldTickets + " tickets \n" +
+	    		"Total " sellConcertInstance.balanceOfTickets(sellContractAddress) + "tickets is used!\n" +
+	    		"Claim " log.args.amount + " ether \n" +
+	    		"Your event is " + logs.args.stateName + "now.\n"
+	    		);
+	    	}
+	    });
+*/
+	},
+});
+
+}
