00bd41e3c6b6e2365855904a41bee5613d28376a
Peter Tsai <laputianzen@gmail.com>
2016-08-20 18:45:42 +0800

    update Supervise Template

--- app/supervisor/Supervise.js
+++ app/supervisor/Supervise.js
@@ -1,16 +1,24 @@
 if (Meteor.isClient) {
 
+Template.Supervise.helpers({
+	ConcertName: function () {return superviseContractInstance.name();},
+});	
+
 Template.Supervise.events({
 	'click .back': function(event){
-	    	event.preventDefault();
-	        BlazeLayout.render('mainLayout', {main: "PlayerOption"});
+	    event.preventDefault();
+	    BlazeLayout.render('mainLayout', {main: "SelectSuperviseConcert"});
 
 	    },
 	'click .yes':function(event){
-		BlazeLayout.render('mainLayout', {main: "Home"});
+		event.preventDefault();
+		Session.set('confirmSignal',true);
+		BlazeLayout.render('mainLayout', {main: "ConfirmEvent"});
 	},
 	'click .no': function(event){
-		BlazeLayout.render('mainLayout', {main: "Home"});
+		event.preventDefault();
+		Session.set('confirmSignal',false);
+		BlazeLayout.render('mainLayout', {main: "ConfirmEvent"});
 	}
 });
 
