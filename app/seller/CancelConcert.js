4657b39ac31d65d60f4708021ea7c4dbed2b0ebd
Peter Tsai <laputianzen@gmail.com>
2016-08-20 13:43:32 +0800

    Add CancelConcert Functionality

---
+++ app/seller/CancelConcert.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+	Template.CancelConcert.events({
+	'submit form': function(event){
+	    	event.preventDefault();
+			var account = web3.eth.accounts[0];
+			var password = event.target.password.value;
+	    	if(! web3.personal.unlockAccount(account,password)){
+				alert('Account ' + account + 'cannot unlock');
+			} else {
+				alert('Account unlock!!!');
+			}			
+
+	    	sellContractInstance.cancelConcert({},{from: account, gas: 100000});
+	        
+	        /*sellContractInstance.CancelConcert({},{address:account}).watch(function(error, log){
+	    		if (!error) {
+	    			alert("Contract " + log.args.thisContract + "is " log.args.stateName " \n");
+	    		}
+	    	});*/
+	        BlazeLayout.render('mainLayout', {main: "Home"});
+
+	    },
+	'click .back': function(event){
+	    	event.preventDefault();
+	        BlazeLayout.render('mainLayout', {main: "YourExistingConcert"});
+
+	    },		
+	});
+}
